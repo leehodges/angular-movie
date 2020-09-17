88316541dc1f9c904aeff51e8c284f77d87ccdf3
Lee Hodges <leehodges@gmail.com>
2020-09-17 16:06:59 -0500

    User Signup Page

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { MustMatch } from "./must-match.validator";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit, OnDestroy {
+  form: FormGroup
+  formValues: any
+  submitting = false
+  hasError = false
+  errorMsg: string
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.createFormControls()
+    this.createForm()
+  }
+
+  createFormControls() {
+    this.formValues =  {
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      nickName: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(10)])],
+      passwordConfirmation: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(10)])]
+    }
+  }
+
+  createForm() {
+    this.form = this.fb.group(this.formValues, { validator: MustMatch('password', 'passwordConfirmation')})
+  }
+
+  // conveience getter method for form controls
+
+  get f() {
+    if (this.form && this.form.controls) {
+      return this.form.controls
+    }
+  }
+  submitForm() {
+
+    this.hasError = false
+    this.submitting = true
+    if (this.form.invalid) {
+      this.hasError = true
+      this.submitting = false
+      return
+    }
+  }
+
+  cancelForm() {
+    this.form.reset()
+  }
+
+  ngOnDestroy() {
+
+  }
+}
